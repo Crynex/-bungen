428d996e0aac3559cdac9b0703754ff11ee6e01b
Crynex <alex2908ber@gmail.com>
2020-07-15 13:06:25 +0200

    Upload

---
+++ Üben Dateien/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Dateien_üben
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Geben Sie den Namen der Datei an: ");
+            string dateiname = Console.ReadLine();
+
+            Console.Write("Geben Sie Ihren Namen an:");
+            string name = Console.ReadLine();
+
+            List<int> zahlen = new List<int>();
+
+            Console.Write("Bitte geben Sie eine Anzahl ein: ");
+            int anzahl = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+
+            for(int i = 0; i < anzahl; ++i)
+            {
+                Console.Write("Bitte geben Sie eine Zahl ein: ");
+                zahlen.Add(Convert.ToInt32(Console.ReadLine()));
+                Console.WriteLine();
+            }
+
+            //Datei öffnen
+            StreamWriter writer = new StreamWriter(dateiname);
+
+            //Schreiben
+            writer.WriteLine(name);
+            for(int i = 0; i < zahlen.Count; ++i)
+            {
+                writer.WriteLine(zahlen[i]);
+            }
+
+            writer.Close();
+        }
+    }
+}
---
+++ Üben Dateien/Zahlen lesen.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dateien_üben
+{
+    class Zahlen_lesen
+    {
+    }
+}
